74224b870914734de922c996e9cae5544cc4d240
Yamahara Yoshihiro <yoshihiro@yamahara.email>
2025-10-26 07:37:35 +0900

    Ladleがビルドされていないのを修正

---
+++ docs/manual/LadleStories/assets/badge-DTjWwp1k.js
@@ -0,0 +1,1 @@
+import{b as e}from"./h-stack-B2sxJFo9.js";const{withContext:t}=e({key:"badge"}),a=t("span");export{a as B};
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// 環境変数 LADLE_ENV でLadleモードかどうかを判断
+const isLadle = process.env.LADLE_ENV === "true";
+
 export default defineConfig({
   base: "./",
   plugins: [
-    tsconfigPaths({ projects: ["tsconfig.app.json"] }), // ←追加
+    tsconfigPaths(
+      isLadle
+        ? { ignoreConfigErrors: true } // Ladle用
+        : { projects: ["tsconfig.app.json"] }, // 通常Viteビルド用
+    ),
     react(),
   ],
   build: { outDir: "./docs" },
